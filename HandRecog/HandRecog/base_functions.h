aaf0379bf96286fd8c0dba196e12305dc8383c8f
mtunique <taomeng@hustunique.com>
2014-05-24 13:09:07 +0800

    add base_functions

---
+++ HandRecog/HandRecog/base_functions.h
@@ -0,0 +1,13 @@
+#ifndef BASE_FUNCTIONS_H
+#define BASE_FUNCTIONS_H
+
+double MAXTAN = 1000000000;
+
+class HandPoint;
+
+double m_tan(HandPoint &, HandPoint &);
+double distance(HandPoint &, HandPoint &);
+
+
+
+#endif BASE_FUNCTIONS_H
